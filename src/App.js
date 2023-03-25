caa2bc45ce571e6f9a26b943a51f14ad346717fc
MarioTiara <mario.tiarapratama@gmail.com>
2023-03-25 22:41:23 +0700

    [20-Styling and css Basic] css module

--- src/App.js
+++ src/App.js
@@ -13,13 +13,16 @@ import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
 import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
-
+import './appStyle.css'
+import styles from './appStyle.module.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Inline/>
+        <h1 className='error'>Error</h1>
+        <h1 className={styles.success}>Success</h1>
+        {/* <Inline/> */}
         {/* <Stylesheet primary={true} /> */}
         {/* <NameList/> */}
         {/* <UserGreeting/> */}
