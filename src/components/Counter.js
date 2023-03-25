04bbe172e9fa4c1a7e339c0c85938e8d077b7728
MarioTiara <mario.tiarapratama@gmail.com>
2023-03-25 13:46:03 +0700

    [11-setStae] using prevSate value

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,17 +10,31 @@ class Counter extends Component {
     }
     increase(){
         // NOTE => this.setState is an async method so if you want to execute a code after this.setState is finished, you gotta use claback function
-        this.setState({
-            count:this.state.count+=1
-        },()=> {
-            console.log('calback value: ', this.state.count);
-        })
+        // this.setState({
+        //     count:this.state.count+=1
+        // },()=> {
+        //     console.log('calback value: ', this.state.count);
+        // })
+
+        this.setState(prevState=>({
+            count:prevState.count+1
+        }))
+
+        console.log(this.state.count);
+    }
+
+    increamentFive(){
+        this.increase();
+        this.increase();
+        this.increase();
+        this.increase();
+        this.increase();
     }
   render() {
     return (
       <div>
         <h1>Coount - {this.state.count}</h1>
-        <button onClick={()=> this.increase()}>Increament</button>
+        <button onClick={()=> this.increamentFive()}>Increament</button>
       </div>
     )
   }
