a1edee4bd861ecbd3f139a4fc194f467b5d450a7
MarioTiara <mario.tiarapratama@gmail.com>
2023-03-25 13:30:17 +0700

    [11-setStae] using setState to increase a couter value

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import Greet  from './components/Greet';
 import Welcome  from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Message/>
+        <Counter></Counter>
+        {/* <Message/> */}
         {/* <Greet name="mario" herroName="superman">
             <p>This is children props</p>
         </Greet>
