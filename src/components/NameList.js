671c2083c648e30e54b157e421126a4f525a16bb
MarioTiara <mario.tiarapratama@gmail.com>
2023-03-25 21:38:52 +0700

    [19-Index as key]

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Person from './Person';
 
 function NameList() {
+    const names=['Bruce', 'Clark', 'Diana'];
     const persons=[{
         id:1,
         name:'Bruce',
@@ -22,8 +23,9 @@ function NameList() {
         }
     ];
     const personList= persons.map(person => <Person key={person.id} person={person}/>);
+    const nameList=names.map((name, index)=><h2 key={index}>{index}. {name}</h2>)
 
-    return <div>{personList}</div>
+    return <div>{nameList}</div>
 }
 
 export default NameList
