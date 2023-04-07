ff5cc94f12e6b6adebee51f18e1e78032515b4d2
MarioTiara <mario.tiarapratama@gmail.com>
2023-04-07 15:53:37 +0700

    [25] FRAGMENT

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greet  from './components/Greet';
-import Welcome  from './components/Welcome';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
 import './appStyle.css'
 import styles from './appStyle.module.css'
 import Form from './components/Form';
 import LifeCycleA from './components/LifeCycleA';
+import FragmentDemo from './components/FragmentDemo';
+import Table from './components/Table';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <LifeCycleA/>
+       <Table></Table>
       </div>
     );
   }
---
+++ src/components/Column.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Column() {
+    return (
+        // <React.Fragment>
+        //     <td>Name</td>
+        //     <td>Mario</td>
+        // </React.Fragment>
+
+        <>
+            <td>Name</td>
+          <td>Mario</td>
+        </>
+    )
+}
+
+export default Column
