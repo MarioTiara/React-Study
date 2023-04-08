732a63f42a5e2324f84d4b9ec63051f843369d33
MarioTiara <mario.tiarapratama@gmail.com>
2023-04-09 06:10:47 +0700

    [29] Refs with Class Components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import './appStyle.css'
 import styles from './appStyle.module.css'
 import RefsDemo from './components/RefsDemo';
+import FocusInput from './components/FocusInput';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <RefsDemo/>
+       <FocusInput/>
       </div>
     );
   }
