0b555b1436931d7da8f7bb0c1ec0e3d5e39e7723
MarioTiara <mario.tiarapratama@gmail.com>
2023-04-09 05:45:30 +0700

    [28] Refs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './appStyle.css'
 import styles from './appStyle.module.css'
-import Form from './components/Form';
-import LifeCycleA from './components/LifeCycleA';
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
-import PureComp from './components/PureComp';
-import ParentRegComponent from './components/ParentRegComponent';
+import RefsDemo from './components/RefsDemo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <ParentRegComponent></ParentRegComponent>
+       <RefsDemo/>
       </div>
     );
   }
