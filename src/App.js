7c3ef95da794eb7c0dbd94b64e3a4140467184ff
MarioTiara <mario.tiarapratama@gmail.com>
2023-04-09 11:01:58 +0700

    [32] Error Boundary

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import './App.css';
 import './appStyle.css'
 import styles from './appStyle.module.css'
 import PortalDemo from './components/PortalDemo';
+import Hero from './components/Hero';
+import ErroBoundary from './components/ErroBoundary';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <PortalDemo></PortalDemo>
+        <ErroBoundary>
+          <Hero heroName="Batman" />
+        </ErroBoundary>
+        <ErroBoundary>
+          <Hero heroName="Superman" />
+        </ErroBoundary>
+        <ErroBoundary>
+          <Hero heroName="Joker" />
+        </ErroBoundary>
+
       </div>
     );
   }
