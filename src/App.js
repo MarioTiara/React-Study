0f57131a6933659d25487915e610edb1b587526d
MarioTiara <mario.tiarapratama@gmail.com>
2023-04-09 11:31:21 +0700

    [33] Higher Order COmponents'

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import './appStyle.css'
 import styles from './appStyle.module.css'
-import PortalDemo from './components/PortalDemo';
-import Hero from './components/Hero';
-import ErroBoundary from './components/ErroBoundary';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ErroBoundary>
-          <Hero heroName="Batman" />
-        </ErroBoundary>
-        <ErroBoundary>
-          <Hero heroName="Superman" />
-        </ErroBoundary>
-        <ErroBoundary>
-          <Hero heroName="Joker" />
-        </ErroBoundary>
-
+        <ClickCounter/>
+        <HoverCounter/>
       </div>
     );
   }
